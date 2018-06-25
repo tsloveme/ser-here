303758a235540e9aedbabbae78c525adf53464cd
chentangsong <472960087@qq.com>
2018-06-26 01:56:25 +0800

    new version release

---
+++ resources/data/server.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    port:8008, // not work yet
+
+    // see https://github.com/chimurai/http-proxy-middleware
+    // localhost:8000/mockdata/user/search  =>  https://www.easy-mock.com/mock/5aef20aefa30186ca1e99750/user/search
+    proxy:[
+        {
+            path: '/mockdata',
+            options: {
+                target:'https://www.easy-mock.com',
+                pathRewrite:{
+                    '^/mockdata' : '/mock/5aef20aefa30186ca1e99750'
+                },
+                changeOrigin: true,
+            }
+        },
+        {
+            path: '/mysearch',
+            options: {
+                target:'https://www.google.com',
+                pathRewrite:{
+                    '^/mysearch' : ''
+                },
+                changeOrigin: false,
+            }
+        }
+    ]
+};
